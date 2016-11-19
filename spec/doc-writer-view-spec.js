34de7d06118599cde552a55fb5dcb581b8e738af
Alan Pich <Alan Pich>
2016-11-19 21:06:03 +0000

    genesis

---
+++ spec/doc-writer-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import DocWriterView from '../lib/doc-writer-view';
+
+describe('DocWriterView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
